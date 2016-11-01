82bbe241c45a68d096e2e41f642d6095db691f45
Paul Thomas <paul94thomas@gmail.com>
2016-11-01 18:34:40 +0530

    updated interface and ai logic

--- Project_7_Tic_Tac_Toe/scripts/app.js
+++ Project_7_Tic_Tac_Toe/scripts/app.js
@@ -17,31 +17,15 @@ function gameLoop(userChoice) {
     var winner = "";
     var draw = false;
     var count = 0;
-    var aiChar = "";
-    var firstTime = true;
-
-    if(userChoice.trim().localeCompare("X") == 0) {
-        console.log("userChoice is X");
-        aiChar = "O";
-    } else {
-        console.log("userChoice is O");
-        aiChar = "X";
-    }
-    console.log("assigned aiChar = " + aiChar);
 
     $(".stage-col").unbind().click(function() {
         //console.log("user has clicked the interface");
         var pos = $(this).data("num");
         var xPos = Math.floor(pos%3);
         var yPos = Math.floor(pos/3);
-        //console.log("pos", pos, "\nxPos", xPos, "yPos", yPos, "\nbefore assigning",
-        //gameStage);
         gameStage[xPos][yPos] = userPlayer;
-        //console.log("after assigning userPlayer");
-        //console.log("gameStage = ", gameStage,"firstTime",  firstTime);
         $(this).text(userChoice);
         if(firstTime) {
-            //console.log("firstTime condition has been fulfilled");
             if(gameStage[1][1] === empty) {
                 console.log("inside here");
                 gameStage[1][1] = aiPlayer;
@@ -52,24 +36,100 @@ function gameLoop(userChoice) {
             do {
                 row = Math.floor(Math.random() * 3);
                 col = Math.floor(Math.random() * 3);
-                //console.log("inside do while");
-                //console.log("row", "xPos", row, xPos, "col", "yPos", col, yPos);
             } while(col == yPos);
             gameStage[row][col] = aiPlayer;
             updateInterface(row, col, aiChar);
             }
             firstTime = false;
+            count++;
         } else {
-            //console.log("Take a look at the gameStage");
-            //console.log(gameStage);
-            //console.log("Calling the AI");
             var tempArr = ai();
-            //console.log("after callin AI");
-            //console.log("row", tempArr[0], "col", tempArr[1]);
-            gameStage[tempArr[0]][tempArr[1]] = aiPlayer;
+            if(count < 4) {
+                gameStage[tempArr[0]][tempArr[1]] = aiPlayer;
+            }
             updateInterface(tempArr[0], tempArr[1], aiChar);
+            count++;
+            console.log(count);
+            if(didWin() === "ai") {
+                console.log("ai wins");
+                announceTheWinner("ai");
+            } else if(didWin() === "user") {
+                console.log("user wins");
+                announceTheWinner("user");
+            } else if(didWin() === "draw" && count == 4) {
+                console.log("It's a draw!");
+                announceTheWinner("draw");
+            }
         }
     });
+}
+
+function didWin() {
+    //for rows
+    for(var i = 0; i < 3; i++) {
+        if(gameStage[i][0] == gameStage[i][1]
+            && gameStage[i][1] == gameStage[i][2]) {
+                //console.log("for rows");
+                if(gameStage[i][0] == aiPlayer) {
+                    return "ai";
+                }
+            else if(gameStage[i][0] == userPlayer) {
+                return "user";
+            }
+                //return -10;
+        }
+    }
+
+    //for columns
+    for(var i = 0; i < 3; i++) {
+        if(gameStage[0][i] == gameStage[1][i]
+            && gameStage[1][i] == gameStage[2][i]) {
+                //console.log("for cols");
+                if(gameStage[0][i] == aiPlayer) {
+                    return "ai";
+                }
+                else if(gameStage[0][i] == userPlayer) {
+                    return "user";
+                }
+            }
+    }
+
+    //check for diagonals
+    if(gameStage[0][0] == gameStage[1][1]
+        && gameStage[1][1] == gameStage[2][2]) {
+            //console.log("for diagnols");
+            if(gameStage[0][0] == aiPlayer) {
+                return "ai";
+            }
+            else if(gameStage[0][0] == userPlayer) {
+                return "user";
+            }
+        }
+
+    if(gameStage[0][2] == gameStage[1][1]
+        && gameStage[1][1] == gameStage[2][0]) {
+            //console.log("for diagnonls");
+            if(gameStage[0][2] == aiPlayer) {
+                return "ai";
+            }
+            else if(gameStage[0][2] == userPlayer) {
+                return "user";
+            }
+        }
+
+
+    return "draw";
+}
+function reset() {
+    for(var i = 0; i < 3; i++) {
+        for(var j = 0; j < 3; j++) {
+            gameStage[i][j] = empty;
+        }
+    }
+    console.log(gameStage);
+    drawCount = 0;
+    firstTime = true;
+    clearInterface();
 }
 function ai() {
     var row = -1;
@@ -94,8 +154,6 @@ function ai() {
 }
 function minimax(depth, isMax) {
     var score = evaluate(depth);
-    //console.log("In Minimax function");
-    //console.log("The score = " + score);
     if(score >= 10) {
         return score;
     }
--- Project_7_Tic_Tac_Toe/scripts/ui.js
+++ Project_7_Tic_Tac_Toe/scripts/ui.js
@@ -6,6 +6,13 @@ var aiPlayer = -2;
 var empty = -1;
 var userMadeMove = false;
 var drawCount = 0;
+var userScore = 0;
+var aiScore = 0;
+var drawScore = 0;
+var userChoice = "";
+var aiChar = "";
+var firstTime = true;
+
 var gameStage = [[empty, empty, empty],
 [empty, empty, empty],
 [empty, empty, empty]];
@@ -14,15 +21,21 @@ var gameStage = [[empty, empty, empty],
 $(document).ready(function() {
 	var user = "";
 	var computer = "";
-	console.log("this is your life");
+
+    $(".winner-announcer").hide();
 
 	//Prompt the user for X or O and store it
 	$(".options-box").fadeIn("slow");
 	$(".x-or-o").click(function() {
-        user = $(this).text();
-        console.log("user = " + user);
+        userChoice = $(this).text();
+        console.log("userChoice = " + userChoice.trim());
+        if(userChoice.trim().localeCompare("X") == 0) {
+            aiChar = "O";
+        } else {
+            aiChar = "X";
+        }
         $(".options-box").fadeOut("slow");
-        gameLoop(user);
+        gameLoop(userChoice);
 	});
 	//Prompt if the user wants to start first and store it
     //or let the user start first
@@ -36,4 +49,57 @@ function updateInterface(row, col, aiChar) {
     var number = col * 3 + row;
     console.log("number = " + number);
     $('*[data-num="'+number+'"]').text(aiChar);
+}
+
+function clearInterface() {
+    for(var i = 0; i < 9; i++) {
+        $('*[data-num="'+i+'"]').text("");
+    }
+}
+
+function announceTheWinner(winner) {
+    if(winner === "ai") {
+        updateScore(winner);
+        updateScoreInterface(winner);
+        announcerBox("desktop", "I WIN");
+    } else if(winner === "user") {
+        updateScore(winner);
+        updateScoreInterface(winner)
+        announcerBox("user", "YOU WON");
+    } else if(winner === "draw") {
+        updateScore(winner);
+        updateScoreInterface(winner);
+        announcerBox("refresh", "RETRY");
+    }
+}
+
+function announcerBox(whoWon, text) {
+    $(".winner-announcer").fadeIn(2000);
+    $(".winner-announcer").html('<i class="fa fa-'+whoWon+' aria-hidden="true"></i><br/>'+text+'');
+    $(".winner-announcer").click(function() {
+        $(this).fadeOut(1000);
+        reset();
+    })
+}
+
+function updateScore(whichScore) {
+    if(whichScore === "user") {
+        userScore++;
+    } else if(whichScore === "ai") {
+        aiScore++;
+    } else if(whichScore === "draw") {
+        drawScore++;
+    }
+    console.log("All scores", userScore, aiScore, drawScore);
+}
+
+function updateScoreInterface(whichScore) {
+    console.log("updateScoreInterface = " + whichScore);
+    if(whichScore === "user") {
+        $(".user-points").text(userScore);
+    } else if(whichScore === "ai") {
+        $(".computer-points").text(aiScore);
+    } else if(whichScore === "draw") {
+        $(".draw-points").text(drawScore);
+    }
 }
